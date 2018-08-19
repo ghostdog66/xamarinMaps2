17038431e99c7ac0f7bc944387015d6d4634d43b
Ghostdog66 <ppen_llamas@hotmail.com>
2018-08-19 19:53:14 +0200

    First Commit

---
+++ XamarinLatinoMaps/XamarinLatinoMaps.UWP/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+namespace XamarinLatinoMaps.UWP
+{
+    public sealed partial class MainPage
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            // Plugin initialization here
+            Xamarin.FormsMaps.Init("fugTOjzqZ4SnhpyODgwS~EicHDmhjc-1-qFFRcnRLag~AubgjSbPJ-WHHW2Q2sFpREnx0TE0hcqOkwJUX01WFdxDEFeUDD2TAF0CQqtEb0jf");
+
+            LoadApplication(new XamarinLatinoMaps.App());
+        }
+    }
+}
---
+++ XamarinLatinoMaps/XamarinLatinoMaps/MainPage.xaml.cs
@@ -0,0 +1,163 @@
+﻿using Newtonsoft.Json;
+using Plugin.Geolocator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Maps;
+using XamarinLatinoMaps.Models;
+
+namespace XamarinLatinoMaps
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+            MapView.MoveToRegion(
+               MapSpan.FromCenterAndRadius(
+                   new Position(37.379672, -5.994488), Distance.FromKilometers(2)));
+
+        }
+
+
+        private void Street_OnClicked(object sender, EventArgs e)
+        {
+            MapView.MapType = MapType.Street;
+        }
+
+
+        private void Hybrid_OnClicked(object sender, EventArgs e)
+        {
+            MapView.MapType = MapType.Hybrid;
+        }
+
+        private void Satellite_OnClicked(object sender, EventArgs e)
+        {
+            MapView.MapType = MapType.Satellite;
+        }
+
+        private async void OnButtonClicked(object sender, EventArgs e)
+        {
+            var locator = CrossGeolocator.Current;
+            locator.DesiredAccuracy = 30;
+
+            var position1 = await locator.GetPositionAsync(TimeSpan.FromSeconds(10));
+
+            LogitudeLabel.Text = position1.Longitude.ToString();
+
+            LatitudeLabel.Text = position1.Latitude.ToString();
+
+            MapView.MoveToRegion(
+               MapSpan.FromCenterAndRadius(
+                   new Position(position1.Latitude, position1.Longitude), Distance.FromKilometers(0.5)));
+
+            var pin = new Pin()
+            {
+                Position = new Position(position1.Latitude, position1.Longitude),
+                Label = "Hola amigos",
+                Address = "Hola amigos 2",
+                Type = PinType.Place,
+                Id = "Xamarin1",
+               
+
+
+            };
+            MapView.Pins.Add(pin);
+
+        }
+
+        private async void LoadApi(object sender, EventArgs e)
+        {
+
+            var client = new HttpClient();
+            client.BaseAddress = new Uri("https://restcountries.eu");
+            var url = "/rest/v2/all";
+
+
+            var response = await client.GetAsync(url);
+
+
+            var answer = await response.Content.ReadAsStringAsync();
+
+
+            var list2 = JsonConvert.DeserializeObject<List<Land>>(answer);
+
+
+            List<Land> solucion = new List<Land>(list2);
+
+            Land tierra = solucion[0];
+
+            var pin = new Pin()
+            {
+                Position = new Position(tierra.Latlng[0], tierra.Latlng[1]),
+                Label = tierra.Name,
+                Address = tierra.Capital,
+                Type = PinType.Place,
+                Id = "Xamarin",
+               
+
+            };
+            MapView.Pins.Add(pin);
+
+        }
+
+
+        private async void FullLoadApi(object sender, EventArgs e)
+        {
+
+            var client = new HttpClient();
+            client.BaseAddress = new Uri("https://restcountries.eu");
+            var url = "/rest/v2/all";
+
+
+            var response = await client.GetAsync(url);
+
+
+            var answer = await response.Content.ReadAsStringAsync();
+
+
+            var list2 = JsonConvert.DeserializeObject<List<Land>>(answer);
+
+
+            List<Land> solucion = new List<Land>(list2);
+
+            Land tierra;
+
+            var pin = new Pin()
+            {
+                Position = new Position(0, 0),
+                Label = "Pin de prueba",
+                Address = "Dirección de prueba",
+                Type = PinType.Place,
+                Id = "Xamarin",
+                
+
+            };
+
+            for (int i = 0; i < 30; i++)
+            {
+
+                tierra = solucion[i];
+
+
+                pin = new Pin()
+                {
+                    Position = new Position(tierra.Latlng[0], tierra.Latlng[1]),
+                    Label = tierra.Name,
+                    Address = tierra.Capital,
+                    Type = PinType.Place,
+                    Id = "Xamarin",
+                   
+
+                };
+                MapView.Pins.Add(pin);
+
+            }
+
+        }
+    }
+}
